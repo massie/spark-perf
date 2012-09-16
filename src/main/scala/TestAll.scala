d71579f9b755eff8eec6a4d5c71d7d8ec765ac37
Paul Ruan <paul.ruan@gmail.com>
2012-09-16 20:31:02 +0000

    added TestAll for convenience

---
+++ src/main/scala/TestAll.scala
@@ -0,0 +1,37 @@
+import spark.SparkContext
+import spark.SparkContext._
+
+/** For convenience of running groupby and sortby 
+  * with a bunch of different arguments.
+  */
+object TestAll {
+  val pairsKeys = List((1000000,100),
+                         (1000000,10000),
+                         (1000000,1000000))
+  
+  def main(args: Array[String]) {
+    val sc = new SparkContext(args(0),"Test All",System.getenv("SPARK_HOME"),List("Spark_Perf.jar"))
+    val numTasks = args(1).toInt
+    val scale = args(2).toInt
+ 
+    val groupByArgs = pairsKeys.map { case (numPairs,numKeys) =>
+      (numPairs*scale,numKeys*scale)
+    }
+    val sortByArgs = groupByArgs
+
+    val groupByTimes = groupByArgs.map { case (numPairs,numKeys) =>
+      GroupBy.runTest(sc,numPairs,numKeys,numTasks)
+    }.zip(groupByArgs)
+ 
+    val sortByTimes = sortByArgs.map { case (numPairs,numKeys) =>
+      SortBy.runTest(sc,numPairs,numKeys,numTasks)
+    }.zip(sortByArgs)
+    
+    sortByTimes.foreach { case (time,(numPairs,numKeys)) =>
+      println("SortBy with " + numPairs + " pairs, " + numKeys + " keys: " + time + " seconds")
+    }
+    groupByTimes.foreach { case (time,(numPairs,numKeys)) =>
+      println("GroupBy with " + numPairs + " pairs, " + numKeys + " keys: " + time + " seconds")
+    }   
+  }
+}
