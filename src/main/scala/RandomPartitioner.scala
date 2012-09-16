5f46c8d90a11101c0d1d43e0ac65d410f569165c
Paul Ruan <paul291@berkeley.edu>
2012-09-15 23:48:38 -0700

    added RandomPartitioner

---
+++ src/main/scala/RandomPartitioner.scala
@@ -0,0 +1,10 @@
+import scala.util.Random
+import spark.Partitioner
+
+class RandomPartitioner(partitions: Int) extends Partitioner {
+   def numPartitions = partitions
+   def getPartition(key: Any): Int =  {
+     Random.nextInt(numPartitions)
+   }
+}
+
