7ba0cecd1ee32e824ec11df44caca211a9105696
Denny <dennybritz@gmail.com>
2012-10-16 13:00:00 -0700

    Compile automatically against downloaded spark.

--- run.rb
+++ run.rb
@@ -12,10 +12,12 @@ if !File.exists?("#{File.dirname(__FILE__)}/config/config.rb")
 end
 
 # Clone the Git repository and create a branch for the hash
-
 $stderr.puts "Downloading Spark..."
 `git clone git://github.com/mesos/spark.git` unless File.exists?("spark")
 $stderr.puts "Building branch #{ARGV[0]}. This may take a while."
-`cd spark; git checkout -b #{ARGV[0]} #{ARGV[0]}; sbt/sbt products`
+`cd spark; git checkout -b #{ARGV[0]} #{ARGV[0]}; sbt/sbt products; sbt/sbt publish-local`
+# Building the spark-perf code against the downloaded version of Spark
+`cd #{File.dirname(__FILE__)}; sbt/sbt compile`
+
 
 exec "#{File.dirname(__FILE__)}/script/all.rb"
