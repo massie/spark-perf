b9764b44f8bfee9252e13f8a105baec41ce74620
Denny <dennybritz@gmail.com>
2012-10-17 19:28:51 -0700

    Bug in executing sync command.

--- run.rb
+++ run.rb
@@ -25,7 +25,7 @@ $stderr.puts `cd #{File.dirname(__FILE__)}; sbt/sbt compile`
 
 # Sync the whole directory to the slaves. 
 # Here we are assuming we are using our Amazon EC2 AMI, that's a TODO.
-$stderr.puts "/root/mesos-ec2/copy-dir #{File.dirname(__FILE__)}"
+$stderr.puts `/root/mesos-ec2/copy-dir #{File.dirname(__FILE__)}`
 
 # Run the tests
 exec "#{File.dirname(__FILE__)}/script/all.rb"
