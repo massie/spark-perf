fcb460adf3cb5c91c4bcb3dc2b3d14a4930dafc9
Denny <dennybritz@gmail.com>
2012-11-11 13:28:55 -0800

    Allow to skip warmup.

--- run.rb
+++ run.rb
@@ -30,18 +30,19 @@ $stderr.puts `cd #{File.dirname(__FILE__)}; sbt/sbt compile`
 $stderr.puts `/root/mesos-ec2/copy-dir #{File.dirname(__FILE__)}` rescue 
   "~/mesos-ec2/copy-dir not found. Continuing without..."
 
-
-# Warmup - write some data to Spark's tmp directory
-require "#{File.dirname(__FILE__) + "/config/config.rb"}"
-if JAVA_OPTS["spark.local.dir"]
-  JAVA_OPTS["spark.local.dir"].split(",").map {|x| x.chomp}.each do |path|
-    $stderr.puts "Warmup - Writing random data to #{path}."
-    $stderr.puts `mkdir #{path}`
-    $stderr.puts `dd if=/dev/random of=#{path}/random bs=#{1024*1024} count=4000`
-    $stderr.puts `/root/mesos-ec2/copy-dir #{path}`
+if (ENV["SKIP_WARMUP"] != "1")
+  # Warmup - write some data to Spark's tmp directory
+  require "#{File.dirname(__FILE__) + "/config/config.rb"}"
+  if JAVA_OPTS["spark.local.dir"]
+    JAVA_OPTS["spark.local.dir"].split(",").map {|x| x.chomp}.each do |path|
+      $stderr.puts "Warmup - Writing random data to #{path}."
+      $stderr.puts `mkdir #{path}`
+      $stderr.puts `dd if=/dev/random of=#{path}/random bs=#{1024*1024} count=4000`
+      $stderr.puts `/root/mesos-ec2/copy-dir #{path}`
+    end
+  else
+    $stderr.puts "Warning: spark.local.dir not set. Not performing disk warmup."
   end
-else
-  $stderr.puts "Warning: spark.local.dir not set. Not performing disk warmup."
 end
 
 # Run the tests
