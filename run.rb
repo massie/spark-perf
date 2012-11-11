aa3025f9dee8cbf11903758e660987b9afc7945e
Denny <dennybritz@gmail.com>
2012-11-11 13:23:19 -0800

    Create tmp directory if it doesn't exist.

--- run.rb
+++ run.rb
@@ -36,7 +36,8 @@ require "#{File.dirname(__FILE__) + "/config/config.rb"}"
 if JAVA_OPTS["spark.local.dir"]
   JAVA_OPTS["spark.local.dir"].split(",").map {|x| x.chomp}.each do |path|
     $stderr.puts "Warmup - Writing random data to #{path}."
-    $stderr.puts `dd if=/dev/random of=#{path}/random bs=1m count=4000`
+    $srderr.puts `mkdir #{path}`
+    $stderr.puts `dd if=/dev/random of=#{path}/random bs=#{1024*1024} count=4000`
     $stderr.puts `/root/mesos-ec2/copy-dir #{path}`
   end
 else
